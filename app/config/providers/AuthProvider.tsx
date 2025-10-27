32ccea19337ead25a654dcdcc86649e57c40f34d
MiskatezzZ <miskatezzz@example.com>
2025-10-27 13:01:47 +0530

    miskatezz

--- app/config/providers/AuthProvider.tsx
+++ app/config/providers/AuthProvider.tsx
@@ -1,6 +1,7 @@
+"use client";
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
-import { auth } from '../config/firebaseConfig';
+import { auth } from '../firebaseConfig';
 
 interface AuthContextValue {
   user: User | null;
